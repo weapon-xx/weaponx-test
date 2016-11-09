5dddf0ab9c15053db0dbbf6b5d5ae2c772b75aed
weapon-xx <88180569@qq.com>
2016-11-09 15:14:55 +0800

    'generator'

--- node/async_await.js
+++ node/async_await.js
@@ -1,12 +1,12 @@
 const git = require('shell').git;
 
 (async function(){
-  const commitMsg = '...';      
+  const commitMsg = '...';
 
   try{
-    await git.add('pattern/for/some/files/*');
-    await git.commit(commitMsg);
-    await git.push();
+    await setTimeout(()=>{
+      console.log('任务1');
+    },1000);
   }catch(err){
     console.log(err);
   }
