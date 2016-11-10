572fa25b7f180aa91196c4a3b5c3b23d9f7a90c6
weapon-xx <88180569@qq.com>
2016-11-10 15:45:59 +0800

    'update'

--- node/co.js
+++ node/co.js
@@ -3,15 +3,19 @@ co(function* (){
   yield new Promise((resolve,reject) => {
     setTimeout(() => {
       console.log('任务1');
-      resolve('任务1')
+      resolve('任务1成功')
     },1000)
+  }).then( data =>{
+    console.log(data);
   });
 
   yield new Promise((resolve,reject) => {
     setTimeout(() => {
       console.log('任务2');
-      resolve('任务2')
+      resolve('任务2成功')
     },1000)
+  }).then( data =>{
+    console.log(data);
   });
 }).catch((err) => {
   console.log(err);
