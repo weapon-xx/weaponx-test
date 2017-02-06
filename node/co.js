72ba8c0b2f8bee7d2d34e961e0eae05cf6c11ec5
weapon-xx <88180569@qq.com>
2017-02-06 16:04:24 +0800

    update

--- node/co.js
+++ node/co.js
@@ -1,22 +1,33 @@
 const co = require('co');
 co(function* (){
-  yield new Promise((resolve,reject) => {
-    setTimeout(() => {
-      console.log('任务1');
-      resolve('任务1成功')
-    },1000)
-  }).then( data =>{
-    console.log(data);
-  });
+  var msg
+  yield task1().then( data =>{
+    console.log(data)
+    msg = data
+  })
 
-  yield new Promise((resolve,reject) => {
-    setTimeout(() => {
-      console.log('任务2');
-      resolve('任务2成功')
-    },1000)
-  }).then( data =>{
-    console.log(data);
+  yield task2(msg).then( data =>{
+    console.log(data)
   });
 }).catch((err) => {
-  console.log(err);
+  console.log(err)
 })
+
+function task1() {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      console.log('任务1')
+      resolve('任务1成功')
+    },1000)
+  })
+}
+
+function task2(msg) {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      console.log(`任务1信息:${msg}`);
+      console.log('任务2')
+      resolve('任务2成功')
+    },1000)
+  })
+}
