f8eadd3e23631a8b174e61bf47d81497fd17c8bf
weapon-xx <88180569@qq.com>
2017-03-08 15:54:42 +0800

    update

---
+++ node/FE.js
@@ -0,0 +1,12 @@
+const express= require('express')
+const app = express()
+
+app.use(express.static(__dirname))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/FE.html')
+})
+
+app.listen(8000, function() {
+  console.log(`web server running in 8080`);
+})
---
+++ node/csrf-be.js
@@ -0,0 +1,26 @@
+const express= require('express')
+const app = express()
+const bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.get('/login' ,(req, res)=>{
+
+  res.cookie('name', 'xx', { domain: '127.0.0.1', expires: new Date(Date.now() + 900000000)})
+
+  res.end('login success')
+})
+
+app.get('/test', (req, res)=>{
+  console.log(req.hostname)
+  console.log(req.cookies.name)
+
+  res.end('success')
+})
+
+app.listen(8080, function(){
+  console.log(`web server running in 8080`);
+})
