c11feeb6080381b65912f94de1e58f3131c3b558
weapon-xx <88180569@qq.com>
2017-04-11 21:32:13 +0800

    update

--- node/FE.js
+++ node/FE.js
@@ -7,6 +7,6 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/FE.html')
 })
 
-app.listen(8000, function() {
-  console.log(`web server running in 8080`);
+app.listen(8000, function() {a
+  console.log(`web server running in 8000`);
 })
