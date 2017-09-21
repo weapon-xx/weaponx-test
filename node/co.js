3611f3779b28aef1ef6d8060c00dff5857052e79
weapon-xx <88180569@qq.com>
2017-09-22 00:44:09 +0800

    update

--- node/co.js
+++ node/co.js
@@ -1,6 +1,5 @@
 const co = require('co');
 co(function* (){
-
   var msg = yield new Promise((resolve, reject) => {
     setTimeout(() => {
       reject('任务1')
@@ -18,10 +17,6 @@ co(function* (){
 }).catch((err) => {
   console.log(err)
 })
-console.log(2);
-
-
-console.log(1)
 
 function task1() {
   return new Promise((resolve,reject) => {
@@ -41,3 +36,52 @@ function task2(msg) {
     },1000)
   })
 }
+
+
+co(function* () {
+  let res = [
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve(1)
+      }, 1000)
+    }).then(data => {
+      return data
+    }),
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve(2)
+      }, 1800)
+    }).then(data => {
+      return data
+    }),
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve(3)
+      }, 1500)
+    }).then(data => {
+      return data
+    })
+  ]
+  return res
+}).then(data => {
+  data.forEach(func => {
+    func.then(data => {
+      console.log(data)
+    })
+  })
+})
+
+
+const fn = co.wrap(function*() {
+  return yield new Promise(resolve => {
+    setTimeout(() => {
+      resolve(1)
+    }, 1500)
+  }).then(data => {
+    return data
+  })
+})
+
+fn().then(data => {
+  console.log('wrap: ' + data);
+})
