2640ecded2627ff3d67562902dcacfbca4c4b21d
v_xiexunxie <v_xiexunxie@tencent.com>
2018-01-19 16:51:08 +0800

    update promise demo

--- js/Promise_demo.js
+++ js/Promise_demo.js
@@ -1,20 +1,56 @@
 'use strict';
-class myPromise {
+class xxPromise {
   constructor(fn) {
-    const _self = this;
+    const _this = this;
     this.state = 'pending';
+    this.result = null;
+    this.reason = null;
 
     this.resolvedHandler = null;
     this.rejectedHandler = null;
-    fn(this.resolved, this.rejected);
+
+    this._promise = {};
+    this._promise._p = _this;
+    this._promise.then = (resolvedHandler, rejectedHandler) => {
+      switch(_this.state) {
+        case 'pending':
+          _this.resolvedHandler = (typeof resolvedHandler === 'function') ? resolvedHandler : null;
+          _this.rejectedHandler = (typeof rejectedHandler === 'function') ? rejectedHandler : null;
+          break;
+        case 'resolved':
+          if(typeof resolvedHandler === 'function') {
+            return resolvedHandler(_this.result)
+          }
+          break;
+        default:
+          if(typeof rejectedHandler === 'function') {
+            return rejectedHandler(_this.reason);
+          }
+      }
+    }
+
+    function resolved(val) {
+      _this.state = 'resolved';
+      _this.result = val;
+      if(typeof _this.resolvedHandler === 'function') {
+        _this.resolvedHandler(val);
+      }
+    }
+
+    function rejected(err) {
+      _this.state = 'rejected';
+      _this.reason = err;
+      if(typeof _this.rejectedHandler === 'function') {
+        __this.rejectedHandler();
+      }
+    }
+
+    fn(resolved, rejected);
+
+    return this._promise;
   }
+}
 
-
-  resolved(val) {
-
-  }
-
-  rejected(err) {
-
-  }
+xxPromise.all = () => {
+  
 }
