653bc1c705d1eb2f2aceb24052414780582badf6
weapon-xx <88180569@qq.com>
2018-03-19 17:08:35 +0800

    update js demo

--- node/FE.js
+++ node/FE.js
@@ -3,10 +3,10 @@ const app = express()
 
 app.use(express.static(__dirname))
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/FE.html')
 })
 
-app.listen(8000, function() {a
+app.listen(8000, () => {
   console.log(`web server running in 8000`);
 })
