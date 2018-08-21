70cc664bb4ba8f550ffd79d96b8e5ab23eb5b7df
weapon-xx <88180569@qq.com>
2018-08-21 13:28:57 +0800

    update demo

--- node/koa.js
+++ node/koa.js
@@ -3,8 +3,9 @@ const route = require('koa-route')
 
 const app = new koa()
 
-app.use(route.get('/', function *() {
+app.use(route.get('/', ctx => {
   this.body = 'Hello World'
+  ctx.response.body = 'Hello ' + 'xx';
 }))
 
 app.listen(3000)
