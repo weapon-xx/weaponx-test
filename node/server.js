b4169c8bc41c1ceed38007ed8740444d24d7290c
jaccksonX <88180569@qq.com>
2019-06-15 13:41:40 +0800

    feat: update cookie-session demo & add Countdown demo

--- node/server.js
+++ node/server.js
@@ -1,15 +1,39 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const resolve = file => path.resolve(__dirname, file)
-app.use('/dist', express.static(resolve('./views')))
-app.use(express.static('../'));      //引入静态资源文件
+const express = require('express');
+const app = express();
+const path = require('path');
+const resolve = file => path.resolve(__dirname, file);
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
-app.set('static', path.join(__dirname, './views'));
+// 指定静态资源路径
+app.use(express.static('../'));      
+
+// 设置渲染引擎
 app.set('view engine', 'html');
 app.engine('html', require('ejs-mate'));
 
+// 使用 cookieParser 中间件;
+app.use(cookieParser());
+
+// 设置 session
+app.use(session({
+  secret: 'recommand 128 bytes random string', // 建议使用 128 个字符的随机字符串
+  cookie: { maxAge: 60 * 1000 },
+}));
+
 app.get('/', (req, res) => {
+  if (req.session.userId || req.cookies.userId) {
+      res.send('welcome');
+      console.log(`session: ${req.session.userId}`);
+  } else {
+    const sessionId =  1;
+    req.session.userId = sessionId;
+    res.cookie('userId', sessionId, { maxAge: 60 * 1000, singed: true});
+    res.send('First Time Login');
+  }
+});
+
+app.get('/data', (req, res) => {
   res.render('data', {
     arr: [1, 2, 3, 4, 5, 6],
   });
