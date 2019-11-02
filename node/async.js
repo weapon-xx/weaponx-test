46df9743257f4c970d297efc1ded726d7a6b43ef
jaccksonX <88180569@qq.com>
2019-11-02 21:40:27 +0800

    feat: update demo

--- node/async.js
+++ node/async.js
@@ -7,3 +7,47 @@ f().then(v => console.log(v))
 const test = require('../node_modules/npm-test-xx/index')
 
 test()
+
+
+// mark
+async function async1() {
+  console.log(9);
+  setTimeout(() => {
+      console.log(10);
+      new Promise((resolve) => {
+          console.log(1);
+          resolve();
+      }).then(() => {
+          console.log(2);
+      })
+  })
+}
+
+async function async2() {
+  async1();
+  new Promise((resolve) => {
+      console.log(6);
+      setTimeout(() => {
+          console.log(11);
+      })
+      resolve();
+  }).then(() => {
+      console.log(5);
+  })
+}
+
+console.log(8);
+
+async function async3() {
+  await async2();
+  console.log(4);
+  new Promise((resolve) => {
+      console.log(3);
+      resolve();
+  }).then(() => {
+      console.log(7);
+  })
+}
+
+console.log(12);
+async3()
